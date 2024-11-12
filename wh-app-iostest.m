a91a360555e38d77c447d0b768c0fe1440886a64
Fsrad <177759728+iHuydang@users.noreply.github.com>
2024-11-13 01:25:37 +0700

    Update and rename wh-app-ios to wh-app-iostest.m

---
+++ wh-app-iostest.m
@@ -0,0 +1,17 @@
+
+//
+//  WhiteHouseTests.m
+//  WhiteHouseTests
+//
+//  Created by Ryan Rusnak on 10/8/14.
+//  Copyright (c) 2014 mobomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface WhiteHouseTests : XCTestCase
+
+@end
+
+@implementation WhiteHouseTests
